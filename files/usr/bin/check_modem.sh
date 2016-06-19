c75d6d10d1b7fbbe5a7408ae1bda6ebf7b5075e4
Toan Pham <tpham3783@gmail.com>
2016-06-19 01:36:13 -0400

    fix Sierra Wireless modem being in bad state right after sys-upgrade.  Configure LEDs while openwrt is still busy formating JFFS filesystem

---
+++ files/usr/bin/check_modem.sh
@@ -0,0 +1,47 @@
+# This script handles the issue where wwan0 is not 
+# brought up after the modem resets with Telit modems. 
+# This also happens to the Sierra module, so handle reset.
+
+h=$(cat /proc/sys/kernel/hostname)
+if [ "$h" == "AP102B" ]; then
+	[ -f /tmp/.quirk_mc7353 ] && exit 0;
+	# This happens because openwrt is still busy formating JFFS right after sys-upgrade
+	if lsusb | grep "1199:68c0"; then
+		# This must be first boot, mount service maybe running and block LED config. Start LED task
+		[ -f /etc/init.d/led ] && /etc/init.d/led boot
+		at.sh "at!reset"
+		
+		touch /tmp/.quirk_mc7353
+		# wman failover seems to have a problem tracking wan right after sysupgrade
+		mwan3 restart
+
+	fi
+fi
+
+
+modem=$(uci get modem.@modem[0].module)
+
+# This fixup is only for Sierra Wireless Cell Modem
+if [ "$modem" == "MC7350" ]; then
+	# Allow enough time for mwan3 to check interface status, dont do anything
+	if [ ! -f /tmp/.lock_first_modem_check ]; then
+		touch /tmp/.lock_first_modem_check
+		exit 0;
+	fi
+	if mwan3 interfaces | grep "interface wwan1 is online" ; then
+		[ -f /tmp/.lock_sierra_issued_reset ] && rm /tmp/.lock_sierra_issued_reset
+		exit 0;
+	else
+		touch /tmp/.lock_sierra_issued_reset
+		at.sh "at!reset"	
+		exit 0;
+	fi 
+	exit 0;
+fi 
+
+
+if [ -c /dev/cdc-wdm0 ] && ! ifconfig | grep wwan0 > /dev/null; then
+	/etc/init.d/network restart 
+fi
+
+exit 0
